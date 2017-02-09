b7a008f444fa3ca4ffbe9d593c93e95dfc4c25c8
ivan_j4u <ivan_j4u@dfa7303e-5589-49f1-90d5-2ebb3190fee7>
2017-02-09 08:41:12 +0000

    Creating User

---
+++ trunk/Java/JTServer/src/main/java/co/id/aribanilia/jtserver/service/UserService.java
@@ -0,0 +1,36 @@
+package co.id.aribanilia.jtserver.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import co.id.aribanilia.jtserver.dao.UserDao;
+import co.id.aribanilia.jtserver.entity.User;
+
+/**
+ * @author ivan_j4u
+ * Feb 9, 2017
+ */
+@Service
+public class UserService {
+
+	@Autowired
+	private UserDao dao;
+	
+	public List<User> all() {
+		return dao.findAll();
+	}
+	
+	public User getById(String id) {
+		return dao.findById(id);
+	}
+	
+	public User getByName(String name) {
+		return dao.findByName(name);
+	}
+	
+	public void save(User user) {
+		dao.save(user);
+	}
+}
